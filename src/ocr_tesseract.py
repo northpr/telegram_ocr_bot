e24aedf626f5ed4bd16dfa0bbdd5b0c4b1929bf5
northpr <north_patawee@hotmail.com>
2023-04-02 00:18:44 +0700

    improve performance

--- src/ocr_tesseract.py
+++ src/ocr_tesseract.py
@@ -1,8 +1,7 @@
 import cv2
-import numpy as np
 import pytesseract
-import re
 from helper import *
+from datetime import datetime
 
 # Load the image
 random_img_path = random_image("img_test")
@@ -18,16 +17,15 @@ img_processed = img_preprocess(img)
 
 # Apply OCR to the preprocessed image
 text = pytesseract.image_to_string(img_processed, lang="eng")
+clean_text = remove_words(text)
 
 # Extract the reference ID and currency values from the text
-ref_id, money_amt = regex_check(text)
+ref_id, money_amt, full_name = regex_check(clean_text)
 
 # Checking number of list of ref_id and money_amt
-if len(ref_id) == 0:
-    ref_id.append("recheck ref_id")
-if len(money_amt) == 0 or (money_amt[0] <= 50 or money_amt[0] >= 500000):
-    money_amt.append("recheck money_amt")
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     
 # Print the extracted numbers
-print(ref_id)
-print(money_amt)
+response = f"Reference ID: {ref_id}\nMoney Amount: {money_amt}\nSender Name: {full_name}\n\nCurrent time: {current_time}"
+print(response)
