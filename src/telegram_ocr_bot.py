45d9c5f92329451b6969e1ccc6520872833cba52
northpr <north_patawee@hotmail.com>
2023-04-02 00:16:45 +0700

    change from pytesseract to Google Vision API

--- src/telegram_ocr_bot.py
+++ src/telegram_ocr_bot.py
@@ -2,15 +2,25 @@ import telebot
 import os
 from helper import *
 import cv2
+import io
+from google.cloud import vision
 import pytesseract
 from datetime import datetime
 
 bot = telebot.TeleBot("6129188503:AAH3M2mg3m-H-RnXQPvToLkK6uS9uyYD2RM")
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'g_credential.json' # Getting JSON file from Google Cloud
+client = vision.ImageAnnotatorClient()
+
+welcome_msg = """Hi from OCRbot, here's the command you could use
+/reader - starting to use OCR bot
+/report - to report a problem
+/tutorial - for reading how to use this bot"""
+
 # Define the handler function for the "/start" command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome to the OCR bot! Send me an receipt to extract the reference ID and currency values.")
+    bot.reply_to(message, welcome_msg)
 
 @bot.message_handler(commands=["reader"])
 def handle_ocr_image(message):
@@ -28,26 +38,30 @@ def handle_image(message):
     image_file = bot.download_file(file_info.file_path)
 
     try:
-        # Convert the image to a numpy array and process image
-        img = cv2.imdecode(np.frombuffer(image_file, np.uint8), cv2.IMREAD_UNCHANGED)
-        img_processed = img_preprocess(img)
+        # Read the image file
+        with io.BytesIO(image_file) as image_binary:
+            content = image_binary.read()
 
-        # Apply OCR to the preprocessed image
-        text = pytesseract.image_to_string(img_processed, lang="eng")
+        # Construct an image instance
+        image = vision.Image(content=content)
+
+        # Performs OCR on the image file
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+
+        # Extract the text from the response
+        text = texts[0].description
+        clean_text = remove_words(text)
 
         # Extract the reference ID and currency values from the text
-        ref_id, money_amt = regex_check(text)
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        ref_id, money_amt, full_name = regex_check(clean_text)
 
         # Checking number of list of ref_id and money_amt
-        if len(ref_id) == 0:
-            ref_id.append("recheck ref_id")
-        if len(money_amt) == 0 or (money_amt[0] <= 50 or money_amt[0] >= 500000):
-            money_amt.append("recheck money_amt")
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # Create a message to send back to the user
-        response = "Reference ID: {}\nMoney Amount: {}\n\nCurrent time: {}".format(ref_id, money_amt, current_time)
-
+            
+        # Print the extracted numbers
+        response = f"Reference ID: {ref_id}\nMoney Amount: {money_amt}\nSender Name: {full_name}\n\nCurrent time: {current_time}"
         # Send the message back to the user
         bot.reply_to(message, response)
     except Exception as e:
