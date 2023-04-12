04762727372939af34b2c549f34e5cf548f1ff1f
northpr <north_patawee@hotmail.com>
2023-04-12 11:28:16 +0700

    Change text extraction

--- src/ocr_g_vision_api.py
+++ src/ocr_g_vision_api.py
@@ -12,10 +12,10 @@ random_img_path = random_image("img_test")
 print(random_img_path)
 
 img_path = random_img_path
-specific_img_path = "img_test/kbank/kbank_94.jpg"
+specific_img_path = "img_test/ktb/ktb_64.jpg"
 
 # Read the image file
-with io.open(img_path, 'rb') as image_file:
+with io.open(specific_img_path, 'rb') as image_file:
     content = image_file.read()
 
 # Construct an image instance
@@ -27,13 +27,17 @@ texts = response.text_annotations
 
 # Extract the text from the response
 text = texts[0].description
+print(text)
 clean_text = remove_words(text)
 
 # Extract the reference ID and currency values from the text
-ref_id, money_amt, full_name = regex_check(clean_text)
+regex_result = regex_check(clean_text)
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     
 # Print the extracted numbers
-response = f"Reference ID: {ref_id}\nMoney Amount: {money_amt}\nSender Name: {full_name}\n\nCurrent time: {current_time}"
+response = f"[BOT]\n\nรหัสอ้างอิง: {regex_result['ref_id']}\
+                        \nจำนวนเงิน: {regex_result['money_amt']}\
+                        \nผู้ฝาก: {regex_result['full_name']}\
+                        \nเวลาที่ทำรายการ: {regex_result['current_time']}"
 print(response)
