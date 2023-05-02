261deb9eb833b717f3f9e86d4c9735f20de10e10
northpr <north_patawee@hotmail.com>
2023-05-02 14:26:50 +0700

    modify helper for nbs

--- nbs/helper.py
+++ nbs/helper.py
@@ -1,138 +1,127 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
+import io
 import os
+import re
 import random
+from datetime import datetime
+from google.cloud import vision
 
-def img_preprocess(img:np): 
-    """Preprocess image to make letters clearer while removing background
+def perform_ocr(client, image_file):
+    with io.BytesIO(image_file) as image_bin:
+        content = image_bin.read()
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    return texts
+
+def remove_words(ocr_result: str)-> str:
+    remove_words = ["จ่ายบิลสําเร็จ", "วีเพย์", "VPAY","เลขที่รายการ", "ผู้รับเงินสามารถสแกนคิวอาร์โค้ด"
+                    "ธ.กสิกรไทย", "ธ.กสิกรไทย", "จํานวน:", "จำนวน:", "จำนวนเงิน", "ค่าธรรมเนียม:",
+                    "สแกนตรวจสอบสลิป", "จาก", "ไปยัง", "จํานวนเงิน", "จ่ายบิลสำเร็จ", "๒ ", "๒"
+                    "รหัสอ้างอิง", "กรุงไทย", "วันเดือนปีที่ทํารายการ", "ค่าธรรมเนียม", "วันเดือนปีที่ทำรายการ"]
+    pattern = "|".join(remove_words)
+    clean_text = re.sub(pattern, "", ocr_result)
+    return clean_text
+
+def extract_ref_id(ocr_results: str):
+    # Checking ref_id
+    ref_id = [int(x) for x in re.findall(r'20\d{16}', ocr_results)]
+    if len(ref_id) == 0:
+        # Try merging lines if normal regex search is not work
+        lines = ocr_results.split("\n")
+        for i, line in enumerate(lines):
+            next_line = lines[i+1]
+            merged_line = line+next_line
+            ref_id = [int(x) for x in re.findall(r'20\d{16}', merged_line)]
+            if len(ref_id) > 0:
+                break
+    return ref_id[0] if len(ref_id) > 0 else False
+
+def extract_money(ocr_results: str):
+    money_amt = re.findall(r'\b\d{1,3}(?:,\d{3})*\.\d{2}\b', ocr_results)
+    money_amt = [float(f.replace(",", "")) for f in money_amt]
+    return money_amt[0] if len(money_amt) > 0 else False
+
+def extract_full_name(ocr_results: str):
+    pattern = r'(นาย ?(?:[\u0E00-\u0E7F ]+)|นางสาว ?(?:[\u0E00-\u0E7F ]+)|น\.ส\. ?(?:[\u0E00-\u0E7F ]+)|นาง ?(?:[\u0E00-\u0E7F ]+))'
+    full_name = re.findall(pattern, ocr_results)
+
+    return full_name[0] if len(full_name) > 0 else False
+
+def extract_acc_num(ocr_results: str):
+    pattern_acc_number = r'[Xx]{3}[-\w\d]+'
+    account_numbers = re.findall(pattern_acc_number, ocr_results)
+    return account_numbers[0] if len(account_numbers) > 0 else False
+
+def format_ref_id_time(ref_id: int) -> str:
+    try:
+        ref_id_str = str(ref_id)
+        year = ref_id_str[0:4]
+        month = ref_id_str[4:6]
+        day = ref_id_str[6:8]
+        hour = ref_id_str[8:10]
+        minute = ref_id_str[10:12]
+
+        formatted_date_time = f"{hour}:{minute} {day}/{month}/{year}"
+        return formatted_date_time
+    except:
+        return "โปรดตรวจสอบด้วยตัวเองอีกครั้ง"
+
+def regex_check(ocr_results: str)->dict:
+    ref_id = extract_ref_id(ocr_results)
+    money_amt = extract_money(ocr_results)
+    full_name = extract_full_name(ocr_results)
+    acc_number = extract_acc_num(ocr_results)
+
+    mistakes = sum(1 for x in [ref_id, money_amt, full_name, acc_number] if x is False)
+    current_time = datetime.now().strftime("%Y%m%d%H%M")
+
+    return {
+    "ref_id": ref_id if ref_id is not False else "โปรดตรวจสอบด้วยตัวเองอีกครั้ง",
+    "money_amt":  money_amt if money_amt is not False else "โปรดตรวจสอบด้วยตัวเองอีกครั้ง",
+    "full_name": full_name if full_name is not False else "โปรดตรวจสอบด้วยตัวเองอีกครั้ง",
+    "acc_number": acc_number if acc_number is not False else "โปรดตรวจสอบด้วยตัวเองอีกครั้ง",
+    "mistakes": mistakes,
+    "current_time": current_time
+        }
+
+def to_unix_timestamp(timestamp_str: str) -> int:
+    """
+    This function takes the input string, converts it to a datetime object using 
+    the specified format, and then converts the datetime object to a Unix timestamp.
+    You can use this Unix timestamp in Grafana for proper time representation.
 
     Args:
-        img (np): Image file
+        timestamp_str (str): timestamp like 202304272135, 202302210940572991
 
     Returns:
-        img_bin (np): Binary image with clearer letters and removed background
+        int: Timestamp in Unix
     """
-    # Convert the image to grayscale
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if not timestamp_str:
+        return -1
+    
+    try: 
+        timestamp_str = timestamp_str[:12]
+        timestamp_dt = datetime.strptime(timestamp_str, "%Y%m%d%H%M")
+        unix_timestamp = int(timestamp_dt.timestamp())
+        return unix_timestamp
+    except ValueError:
+        return -1
 
-    # Threshold the image to obtain a binary mask of the black letters
-    _, img_bin = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+### JUST FOR LOCAL USE NOT IMPORTANT ###
 
-    # Invert the binary mask to obtain a mask of the background
-    img_bg = cv2.bitwise_not(img_bin)
-
-    # Apply color thresholding to the image to remove the background
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower = np.array([0, 0, 0])
-    upper = np.array([179, 255, 150])
-    img_mask = cv2.inRange(img_hsv, lower, upper)
-
-    # Apply the background mask to the color thresholded image
-    img_masked = cv2.bitwise_and(img_mask, img_mask, mask=img_bg)
-
-    # Invert the binary mask again to obtain the final binary image
-    img_bin_final = cv2.bitwise_not(img_masked)
-
-    # Apply erosion to remove small details and noise from the background
-    kernel = np.ones((2, 2), np.uint8)
-    img_erode = cv2.erode(img_bin_final, kernel, iterations=1)
-
-    # Apply dilation to make the letters thicker
-    # img_dilate = cv2.dilate(img_erode, kernel, iterations=1)
-
-    return img_erode
-
-def random_image(directory: str)->str : 
-    # Get a list of all subdirectories in the given directory
-    subdirectories = [os.path.join(directory, name) for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
-
-    # If there are no subdirectories, return None
-    if not subdirectories:
-        return None
-
-    # Pick a random subdirectory
-    random_subdirectory = random.choice(subdirectories)
-
-    # Get a list of all image files in the random subdirectory and its subdirectories
-    image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif']
+def random_image(directory):
+    # Search for all .jpg files in the specified directory and its subdirectories
     image_files = []
-    for root, dirs, files in os.walk(random_subdirectory):
+    for root, dirs, files in os.walk(directory):
         for file in files:
-            if os.path.splitext(file)[1].lower() in image_extensions:
+            if file.lower().endswith('.jpg'):
                 image_files.append(os.path.join(root, file))
 
     # If there are no image files, return None
     if not image_files:
         return None
 
-    # Pick a random image file and return its path
+    # Pick a random image file from the list of image files found, and return its path
     random_image_path = random.choice(image_files)
     return random_image_path
-
-
-def img_load(bankname: str, filename: str):
-    """Load image to variable
-
-    Args:
-        bankname: bkb, kbank, ksb, scb
-        filename: name of img in the folder
-    """
-    full_dir = "../data/"+ bankname + "/" + filename + ".jpg"
-    img = plt.imread(full_dir)
-    return img
-
-def img_show_high(img, x_ticks:int=30, y_ticks:int=30, x_lim:int=1000, y_lim:int=1000):
-    """Show image in mode detailed of coordinates
-    Example:
-        bank_name, file_name = "kbank", "S__20307981"
-        img_test = img_load(bankname=bank_name, filename=file_name)
-        img_show_high(img_test)
-
-    Args:
-        img (_type_): fill the img in it
-        x_ticks (int, optional): Show how detailed of coordinated should be on x axis. Defaults to 30.
-        y_ticks (int, optional): Show how detailed of coordinated should be on y axis. Defaults to 30.
-        x_lim (int, optional): Limit of x-axis. Defaults to 1000.
-        y_lim (int, optional): Limit of y-axis. Defaults to 1000.
-    """
-    # Show more coordinates
-    x = np.arange(0, x_lim, x_ticks)
-    y = np.arange(0, y_lim, y_ticks)
-    plt.figure(figsize=(14,10))
-    plt.xticks(x)
-    plt.yticks(y)
-    plt.imshow(img);
-
-def get_var_name(variable):
-    """Print the name of variable
-
-    Args:
-        variable (_type_): _description_
-
-    Returns:
-        _type_: String
-        
-    Examples:
-        apple = 3
-        print(get_var_name(apple))
-        -> "apple"
-    """
-    globals_dict = globals()
-    name = [var_name for var_name in globals_dict if globals_dict[var_name] is variable]
-    return name[0]
-
-def show_images(list_of_images:list, rows:int, columns:int, size_x:int=20, size_y:int=20):
-    """
-    Show the list of images
-    Example: show_images(img_list,rows=1,columns=2)
-    """
-    if len(list_of_images)>(rows*columns):
-        return "Check the input"
-    plt.figure(figsize=(size_x, size_y))
-    for i in range(len(list_of_images)):
-        y_shape = list_of_images[i].shape[0]
-        x_shape = list_of_images[i].shape[1]
-        plt.subplot(rows, columns, i+1);plt.imshow(list_of_images[i]);plt.title(f"{get_var_name(list_of_images[i])} | {x_shape}x{y_shape}");
-
 
